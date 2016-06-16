9d0585685d076306fa6f963470ae0fe91ab9bf38
john-dederer <john.dederer@protonmail.ch>
2016-06-16 02:14:59 +0200

    Hello World!

---
+++ Hello Worlds/Hello Worlds/Hello Worlds.cpp
@@ -0,0 +1,15 @@
+// Hello Worlds.cpp : Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Hello World!" << endl;
+    return 0;
+}
+
